e3fb76d5b884cdcbfb6da8c9c6ed5d17834d4ec5
rapidera <rahul.y.dudhane@gmail.com>
2019-01-14 19:35:42 +0530

    "Refactor in css"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import {FlexLayoutModule} from '@angular/flex-layout';
 
-import { MatUiComponents } from './constants/mat-ui-components';
+import { MatUiComponents } from './constants/mat-ui-components.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
