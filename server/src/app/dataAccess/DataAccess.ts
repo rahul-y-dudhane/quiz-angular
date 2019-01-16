89ade2ecab866c7dcf25da4a8a72e5483bc06af3
Udayaditya Singh <udayaditya@rapidera.com>
2019-01-16 12:45:27 +0530

    dataAccess changes

--- server/src/app/dataAccess/DataAccess.ts
+++ server/src/app/dataAccess/DataAccess.ts
@@ -4,23 +4,24 @@ import Constants = require('./../../config/constants/constants');
 class DataAccess {
     static mongooseInstance: any;
     static mongooseConnection: Mongoose.Connection;
-    
-    constructor () {
+
+    constructor() {
         DataAccess.connect();
     }
-    
-    static connect (): Mongoose.Connection {
-        if(this.mongooseInstance) return this.mongooseInstance;
-        
-        this.mongooseConnection  = Mongoose.connection;
+
+    static connect(): Mongoose.Connection {
+        if (this.mongooseInstance) return this.mongooseInstance;
+
+        this.mongooseConnection = Mongoose.connection;
         this.mongooseConnection.once("open", () => {
-            console.log("Conectado ao mongodb.");
+            console.log("Ready to Connect");
+            console.log('Sucessfully Connected to MongoDb');
         });
-        
-       this.mongooseInstance = Mongoose.connect(Constants.DB_CONNECTION_STRING);
-       return this.mongooseInstance;
+
+        this.mongooseInstance = Mongoose.connect(Constants.DB_CONNECTION_STRING);
+        return this.mongooseInstance;
     }
-    
+
 }
 
 DataAccess.connect();
